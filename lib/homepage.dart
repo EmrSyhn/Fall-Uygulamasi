3b9586846e01f5daecc59e2bf050a98125e814ad
emrsyhn <emirseyhana.z@gmail.com>
2022-07-02 22:07:58 +0300

    Düzenlemeler

---
+++ lib/homepage.dart
@@ -0,0 +1,71 @@
+// ignore_for_file: unnecessary_string_interpolations
+
+import 'dart:math';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+Random random = Random();
+
+int randomNumber = random.nextInt(4);
+String stringValue = random.toString();
+
+var gelenYaziIcerigi = "";
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.all(15),
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      '$gelenYaziIcerigi',
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      //button
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color.fromARGB(255, 146, 70, 72),
+        onPressed: yazGetir,
+        tooltip: 'Rastgele Fal',
+        child: const Icon(Icons.done_all),
+      ),
+    );
+  }
+
+  yazGetir() {
+    FirebaseFirestore.instance.collection('Yazilar').doc('3').get().then(
+      (gelenVeri) {
+        setState(
+          () {
+            gelenYaziIcerigi = gelenVeri.data()?['icerik'];
+          },
+        );
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,13 @@
-// ignore_for_file: depend_on_referenced_packages, unnecessary_string_interpolations, unused_import
-
-import 'dart:math';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:fal_app/homepage.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -19,9 +15,11 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      theme: ThemeData(
+          scaffoldBackgroundColor: const Color.fromARGB(255, 250, 210, 218)),
+      home: const MyHomePage(),
     );
   }
 }
@@ -35,94 +33,9 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-Random random = Random();
-
-int randomNumber = random.nextInt(4);
-String stringValue = random.toString();
-
 class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
-  // void _incrementCounter() {
-  //   setState(
-  //     () {
-  //       _counter++;
-  //     },
-  //   );
-  // }
-
-  // String mevcutkullaniciUIDTutucu = "";
-
-  // kullaniciUIDAl() async {
-  //   FirebaseAuth yetki = FirebaseAuth.instance;
-  //   final mevcutkullanici = await yetki.currentUser!;
-
-  //   setState(() {
-  //     mevcutkullaniciUIDTutucu = mevcutkullanici.uid;
-  //   });
-  // }
-
-  // var gelenYaziBasligi = "";
-  var gelenYaziIcerigi = "";
-  yazGetir() {
-    FirebaseFirestore.instance.collection('Yazilar').doc('3').get().then(
-      (gelenVeri) {
-        setState(
-          () {
-            // gelenYaziBasligi = gelenVeri.data()?['baslik'];
-            gelenYaziIcerigi = gelenVeri.data()?['icerik'];
-          },
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 250, 210, 218),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.all(15),
-                padding: const EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                child: Column(
-                  children: [
-                    // ListTile(
-                    //   title: Text(gelenYaziBasligi),
-                    //   subtitle: Text(gelenYaziIcerigi),
-                    // ),
-                    Text(
-                      '$gelenYaziIcerigi',
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    // const SizedBox(
-                    //   height: 20,
-                    // ),
-                    // Text(
-                    //   '$_counter',
-                    //   style: Theme.of(context).textTheme.headline5,
-                    // ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      //button
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color.fromARGB(255, 146, 70, 72),
-        onPressed: yazGetir,
-        tooltip: 'Rastgele Fal',
-        child: const Icon(Icons.done_all),
-      ),
-    );
+    return const HomePage();
   }
 }
